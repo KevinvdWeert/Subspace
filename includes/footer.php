944a723882dca38db3e74d98be4683758ae0b781
KevinvdWeert <Ke.vandeweert@gmail.com>
2026-01-05 14:32:10 +0100

    add footer and main content structure to index page

---
+++ includes/footer.php
@@ -0,0 +1,4 @@
+<footer>
+    <p>&copy; <?php echo date("Y"); ?> Subspace Project. All rights reserved.</p>
+</footer>
+</html>
--- index.php
+++ index.php
@@ -1,3 +1,14 @@
 <?php
 require_once 'includes/header.php';
 ?>
+
+<body>
+    <div class="container">
+        <h1 class="mt-5">Welcome to the Subspace Project</h1>
+        <p class="lead">This is the main page of the Subspace Project website.</p>
+    </div>
+</body>
+
+<?php
+require_once 'includes/footer.php';
+?>
