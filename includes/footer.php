7434f38a553225ad813fdcee0efda1ef947746d0
KevinvdWeert <Ke.vandeweert@gmail.com>
2026-01-07 11:36:54 +0100

    refactor: restructure HTML sections and enhance footer content

--- includes/footer.php
+++ includes/footer.php
@@ -1,4 +1,19 @@
-<footer>
-    <p>&copy; <?php echo date("Y"); ?> Subspace Project. All rights reserved.</p>
-</footer>
+<section class="footer">
+    <footer class="mt-5" >
+        <div class="container text-center">
+            <ul class="list-inline mb-2">
+                <li class="list-inline-item"><a href="#">Rules</a></li>
+                <li class="list-inline-item"><a href="#">Privacy</a></li>
+                <li class="list-inline-item"><a href="#">User Agreement</a></li>
+                <li class="list-inline-item"><a href="#">Accessibility</a></li>
+                <li class="list-inline-item"><a href="#">Contact</a></li>
+                <li class="list-inline-item"><a href="#">Cookies</a></li>
+                <li class="list-inline-item"><a href="#">Terms</a></li>
+            </ul>
+            <p class="mb-0 small text-muted">&copy; <?php echo date('Y'); ?> Subspace Project. All rights reserved.</p>
+        </div>
+    </footer>
+</section>
+</body>
+
 </html>
